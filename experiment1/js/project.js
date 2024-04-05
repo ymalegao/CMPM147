858cee3bb5bc42aa544ce6eef41822dafb735812
Yash <ymalegao@ucsc.edu>
2024-04-05 13:21:50 -0700

    done

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -24,11 +24,92 @@ class MyProjectClass {
 
 function main() {
   // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+    const fillers = {
+      message: [
+        "Hear ye, Hear ye",
+        "Attention, fellow citizens",
+        "My fellow countrymen",
+        "Gentlemen and ladies",
+      ],
+      emotion: ["sad", "appalled", "terrified", "horrified", "mortified"],
+      emotion2: ["happy", "glad", "delighted", "encouraged", "hopeful"],
+      twitterhandle: [
+        "@JamesMadison",
+        "@KingofFrance",
+        "@Englandh8er",
+        "@JohnAdams",
+        "@ThomasJefferson",
+      ],
+      enemies: [
+        "the British",
+        "King George",
+        "foreign invaders",
+        "tyranny",
+        "oppression",
+        "injustice",
+      ],
+      adjective: [
+        "great",
+        "lousy",
+        "young",
+        "developing",
+        "flourishing",
+        "struggling",
+        "prosperous",
+        "free",
+      ],
+      senator: [
+        "@AaronBurr",
+        "@JohnHancock",
+        "@BenjaminFranklin",
+        "@GeorgeWashington",
+        "@PhillipSchuyler",
+      ],
+      tweeter: [
+        "@AlexanderHamilton",
+        "@ThomasPaine",
+        "@JohnJay",
+        "@AbigailAdams",
+        "@MarquisdeLafayette",
+      ],
+    };
+    
+    const template = `$message!
+                      I am $emotion yet $emotion2 by the actions committed by $enemies. We must rally together as a nation.
+                      I beseech thee, $twitterhandle, to stand with me in this hour of need. I seek only the welfare of our $adjective land.
+                      Let it be known to $senator that we shall draft laws to avenge these transgressions.
+                      Your obedient servant,
+                      $tweeter`;
+    
+    // STUDENTS: You don't need to edit code below this line.
+    
+    const slotPattern = /\$(\w+)/;
+    
+    function replacer(match, name) {
+      let options = fillers[name];
+      if (options) {
+        return options[Math.floor(Math.random() * options.length)];
+      } else {
+        return `<UNKNOWN:${name}>`;
+      }
+    }
+    
+    function generate() {
+      let story = template;
+      while (story.match(slotPattern)) {
+        story = story.replace(slotPattern, replacer);
+      }
+    
+      /* global box */
+      $("#box").text(story);
 
-  // call a method on the instance
-  myInstance.myMethod();
-}
+
+    }
+    
+    /* global clicker */
+    $("#clicker").click(generate);    
+    generate();
+  }
 
 // let's get this party started - uncomment me
-//main();
+main();
